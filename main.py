b62e330c7b2f8e27b6d6968cd3ae5f31df7eaa76
alecmaule <maule.alec@gmail.com>
2024-11-09 15:28:37 -0600

    clock and frame rate set up, adding sprites now

--- main.py
+++ main.py
@@ -3,18 +3,32 @@ from constants import *
 
 def main():
     pygame.init()
+    
+    # set up games clock/fps
+    
+    game_clock = pygame.time.Clock()
+    dt = 0
+    
+    
+    # setting up screen size based on constants
+    
     screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
     
+    
+   # print game info 
     print("Starting asteroids!")
     print(f"Screen width: {SCREEN_WIDTH}")
     print(f"Screen height: {SCREEN_HEIGHT}")
     
+    # main game loop, sets quit control, fills screen, and double buffers
     while True:
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 return
+
         screen.fill((0, 0, 0))
         pygame.display.flip()
+        dt = game_clock.tick(60) / 1000
         
 
 
